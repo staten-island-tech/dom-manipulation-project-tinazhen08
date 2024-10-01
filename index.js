244cbe0543347a4dbf5ffbab22c88d4169add6b5
tinazhen08 <158211507+tinazhen08@users.noreply.github.com>
2024-10-01 13:59:49 -0400

    dom selector

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 const DOMSelectors = {
-    
+    header: document.querySelector("h1"),
+    items: document.querySelector("li"),
+    cardHeader: document.querySelector(".card-header"),
+    button: document.querySelector(".btn"),
+    form: document.querySelector(".form")
 };
+
+//Dictionary (python) -> Object (javascript)
+let student = {
+    name: "Ryan",
+    age: 15,
+    girlfriend: true,
+};
+
+DOMSelectors.button.addEventListener("click", function (event) {
+    console.log(event.target.parentElement);
+    event.target.parentElement.style.backgroundColor = "red";
+});
+
+DOMSelectors.form.addEventListener("submit", function (event) {
+    event.preventDefault(); 
+    console.log(document.querySelector("input").value);
+});
