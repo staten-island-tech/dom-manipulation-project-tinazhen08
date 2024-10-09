3c577f1d0ed439bed3976462b2e84a3d9a68e27e
tinazhen08 <158211507+tinazhen08@users.noreply.github.com>
2024-10-09 13:56:50 -0400

    html

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const DOMSelectors = {
     header: document.querySelector("h1"),
-    items: document.querySelector("li"),
     cardHeader: document.querySelector(".card-header"),
     button: document.querySelector(".btn"),
-    form: document.querySelector(".form")
+    form: document.querySelector(".form"),
+    container: document.querySelector(".container")
 };
 
 //Dictionary (python) -> Object (javascript)
@@ -22,3 +22,36 @@ DOMSelectors.form.addEventListener("submit", function (event) {
     event.preventDefault(); 
     console.log(document.querySelector("input").value);
 });
+
+//create the HTML for inputs, cards and container aka where the cards go
+
+//select query the HTML form and get values from inputs
+
+//turn values into object Movie, Game, User, whatevs
+
+//insert card with object onto HTML
+
+//add event listener for remove button in JS
+
+
+//select all buttons as nodelist (can use for each)
+const buttons = document.querySelectorAll("button");
+//made array from buttons if i want to use filter etc.
+const newButtons = Array.from(buttons);
+
+newButtons.forEach((button)=>
+    button.addEventListener("click", function(event){
+        console.log(event.target.textContent);
+    })
+);
+
+let movie = {
+    title: '',
+    release: '',
+};
+
+DOMSelectors.container.insertAdjacentHTML(
+    "beforeend",
+    `<div class="card"><h2 class="card-header">${movie.title}</h2></div>`
+);
+
